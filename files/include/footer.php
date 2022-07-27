13617b4ee65a3e08f9544eaa4fcc9f9d207b7e29
Rohit Anand Vanita shinde <50909065+shinderohit@users.noreply.github.com>
2022-07-27 20:20:54 +0530

    Add files via upload

---
+++ files/include/footer.php
@@ -0,0 +1,38 @@
+	<footer>
+		<div class="footer-logo">
+			<img src="files/img/footer-logo.png">
+		</div>
+		<div class="container footer-list txt-center pt-100">
+			<ul>
+				<li><a href="about.php">About</a></li><span class="hr-1">|</span>
+				<li><a href="">Floorplan</a></li><span class="hr-1">|</span>
+				<li><a href="ameniti.php">Amenities</a></li><span class="hr-1">|</span>
+				<li><a href="gallery.php">Gallery</a></li><span class="hr-1">|</span>
+				<li><a href="neighborhood.php">Neighborhood</a></li><span class="hr-1">|</span>
+				<li><a href="tour.php">Tour</a></li><span class="hr-1">|</span>
+				<li><a href="contact.php">Contact Us</a></li>
+			</ul>
+		</div>
+		<div class="para-content pt-100 container txt-center pb-100">
+			<p>1255 Escalante Dr.</p>
+			<p> Durango, CO 81303</p>
+			<p class="pt-50">(970)422-8300</p>
+			<p>inforocketpointe@nemanagement.net</p>
+			<div class="social-icon">
+				<a href=""><i class="fab fa-facebook-f"></i></a>
+				<a href=""><i class="fab fa-instagram"></i></a>
+				<a href=""><i class="fas fa-snowflake"></i></a>
+			</div>
+		</div>
+	</footer>
+
+	<script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+	<script>
+		    AOS.init();
+	</script>
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+	<script type="text/javascript" src="files/js/slick.min.js"></script>
+	<script type="text/javascript" src="files/js/style.js"></script>
+	
+</body>
+</html>
---
+++ files/include/header.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Rocket Point</title>
+	<link rel="stylesheet" type="text/css" href="files/css/style.css">
+	<link rel="stylesheet" type="text/css" href="files/css/slick.css"/>
+	<link rel="stylesheet" type="text/css" href="slick/slick-theme.css"/>
+	<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" crossorigin="anonymous"/>
+
+</head>
+<body>
+	<nav id="navbar">
+		<div class="flex-just-bwt nav">
+			<div class="nav-menu flex-just-bwt ">
+				<div class="logo">
+					<img src="files/img/logo.png">
+				</div>
+				<div class="menu flex-direction-col"> 
+					<p class="menu-text ml-30 ml-20">menu</p>
+					<div id="myNav" class="overlay">
+					  	<a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+					  	<div class="overlay-content">
+					  		<a href="index.php">Home</a>
+					    	<a href="about.php">About</a>
+					    	<a href="#">Floorplan</a>
+					    	<a href="ameniti.php">Amenities</a>
+					    	<a href="gallery.php">Gallery</a>
+					    	<a href="neighborhood.php">Neighborhood</a>
+					    	<a href="tour.php">Tour</a>
+					    	<a href="contact.php">Contact Us</a>
+					  	</div>
+					</div>
+					<span class="menu-icon ml-30 ml-20" onclick="openNav()"><i class="fal fa-bars"></i></span>
+				</div>
+			</div>
+			<div class="nav-contact">
+				<div class="nav-con">
+					<ul class="con">
+						<li>Call Today! (970) 422-8300</li><span class="hr">|</span>
+						<li>1255 Escalante Drivw</li><span class="hr">|</span>
+						<li>Durango, CO 81303</li>
+					</ul>
+				</div>
+				<div class="flex-just-end pr-50">
+					<button class="apl-btn">Apply now</button>
+				</div>
+			</div>
+		</div>
+	</nav>
+
+	
